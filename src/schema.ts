05de9abf71f6be7c2f5cf6093e0b722891bd60c7
Anjali Modi <anjali.modi@wickes.co.uk>
2023-07-17 15:41:27 +0530

    feat: :sparkles: serverless-pipes-intial-version

---
+++ src/schema.ts
@@ -0,0 +1,317 @@
+const DynamoDbSourceObject = {
+  type: "object",
+  properties: {
+    arn: {
+      anyOf: [
+        {
+          type: "object",
+        },
+        {
+          type: "string",
+        },
+      ],
+    },
+    batchsize: {
+      type: "integer",
+    },
+    deadLetterArn: {
+      anyOf: [
+        {
+          type: "object",
+        },
+        {
+          type: "string",
+        },
+      ],
+    },
+    maximumBatchingWindow: {
+      type: "integer",
+    },
+    maximumRecordAgeInSeconds: {
+      type: "integer",
+      default: -1,
+    },
+    maximumRetryAttempts: {
+      type: "integer",
+      default: -1,
+    },
+    onPartialBatchItemFailure: {
+      type: "string",
+      enum: ["AUTOMATIC_BISECT"],
+    },
+    parallelizationFactor: {
+      type: "integer",
+      default: 1,
+    },
+    startingPosition: {
+      type: "string",
+      enum: ["TRIM_HORIZON", "LATEST"],
+    },
+  },
+  required: ["arn", "startingPosition"],
+} as const;
+
+const KinesisStreamSourceObject = {
+  type: "object",
+  properties: {
+    arn: {
+      anyOf: [
+        {
+          type: "object",
+        },
+        {
+          type: "string",
+        },
+      ],
+    },
+    batchsize: {
+      type: "integer",
+    },
+    deadLetterArn: {
+      anyOf: [
+        {
+          type: "object",
+        },
+        {
+          type: "string",
+        },
+      ],
+    },
+    maximumBatchingWindow: {
+      type: "integer",
+    },
+    maximumRecordAgeInSeconds: {
+      type: "integer",
+      default: -1,
+    },
+    maximumRetryAttempts: {
+      type: "integer",
+      default: -1,
+    },
+    onPartialBatchItemFailure: {
+      type: "string",
+      enum: ["AUTOMATIC_BISECT"],
+    },
+    parallelizationFactor: {
+      type: "integer",
+      default: 1,
+    },
+    startingPosition: {
+      type: "string",
+      enum: ["TRIM_HORIZON", "LATEST", "AT_TIMESTAMP"],
+    },
+    startingPositionTimestamp: {
+      type: "string",
+    },
+  },
+  required: ["arn", "startingPosition"],
+} as const;
+
+const SQSSourceObject = {
+  type: "object",
+  properties: {
+    arn: {
+      anyOf: [
+        {
+          type: "object",
+        },
+        {
+          type: "string",
+        },
+      ],
+    },
+    batchsize: {
+      type: "integer",
+    },
+    maximumBatchingWindow: {
+      type: "integer",
+    },
+  },
+  required: ["arn"],
+} as const;
+
+const SNSTargetObject = {
+  type: "object",
+  properties: {
+    arn: {
+      anyOf: [
+        {
+          type: "object",
+        },
+        {
+          type: "string",
+        },
+      ],
+    },
+  },
+  required: ["arn"],
+} as const;
+
+const LambdaFunctionTargetObject = {
+  type: "object",
+  properties: {
+    arn: {
+      anyOf: [
+        {
+          type: "object",
+        },
+        {
+          type: "string",
+        },
+      ],
+    },
+    invocationType: { type: "string" },
+  },
+  required: ["arn"],
+} as const;
+
+const StepFunctionTargetObject = {
+  type: "object",
+  properties: {
+    arn: {
+      anyOf: [
+        {
+          type: "object",
+        },
+        {
+          type: "string",
+        },
+      ],
+    },
+    invocationType: { type: "string" },
+  },
+  required: ["arn"],
+} as const;
+
+const SQSTargetObject = {
+  type: "object",
+  properties: {
+    arn: {
+      anyOf: [
+        {
+          type: "object",
+        },
+        {
+          type: "string",
+        },
+      ],
+    },
+    messageDeduplicationId: { type: "string" },
+    messageGroupId: { type: "string" },
+  },
+  required: ["arn"],
+} as const;
+
+const FilterParameter = {
+  type: "array",
+  items: {
+    type: "object",
+    properties: {
+      Pattern: { type: "string" },
+    },
+  },
+} as const;
+
+const SourceParameter = {
+  type: "object",
+  oneOf: [
+    {
+      properties: {
+        dynamodb: DynamoDbSourceObject,
+      },
+    },
+    {
+      properties: {
+        kinesisStream: KinesisStreamSourceObject,
+      },
+    },
+    {
+      properties: {
+        sqs: SQSSourceObject,
+      },
+    },
+  ],
+} as const;
+
+const EnrichmentParameter = {
+  type: "object",
+  properties: {
+    name: {
+      anyOf: [
+        {
+          type: "object",
+        },
+        {
+          type: "string",
+        },
+      ],
+    },
+  },
+} as const;
+
+const TargetParameter = {
+  type: "object",
+  oneOf: [
+    {
+      properties: {
+        sns: SNSTargetObject,
+      },
+    },
+    {
+      properties: {
+        lambda: LambdaFunctionTargetObject,
+      },
+    },
+    {
+      properties: {
+        stepFunction: StepFunctionTargetObject,
+      },
+    },
+    {
+      properties: {
+        sqs: SQSTargetObject,
+      },
+    },
+  ],
+} as const;
+
+const IAMRoleParameter = {
+  type: "object",
+  properties: {
+    statements: {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          Effect: { type: "string" },
+          Action: { type: "array" },
+          Resource: { type: "string" },
+        },
+      },
+    },
+  },
+} as const;
+
+export const schema = {
+  type: "object",
+  patternProperties: {
+    "^[a-zA-Z0-9-]+$": {
+      type: "object",
+      properties: {
+        description: {
+          type: "string",
+        },
+        desiredState: {
+          type: "string",
+        },
+        source: SourceParameter,
+        filter: FilterParameter,
+        enrichment: EnrichmentParameter,
+        target: TargetParameter,
+        iamRolePipes: IAMRoleParameter,
+      },
+      required: ["source", "target", "iamRolePipes"],
+      additionalProperties: false,
+    },
+  },
+} as const;
